6119906c8387ba5dc49ad05eecff89382bdbae32
Олгица Мирчева <Olgica.Mircheva@students.finki.ukim.mk>
2021-06-12 13:18:47 +0200

    added possibility to see the score

--- Game/ExitMenu.Designer.cs
+++ Game/ExitMenu.Designer.cs
@@ -32,6 +32,8 @@ namespace Game
             this.label1 = new System.Windows.Forms.Label();
             this.labelScore = new System.Windows.Forms.Label();
             this.panel6 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
             // label1
@@ -51,23 +53,50 @@ namespace Game
             this.labelScore.BackColor = System.Drawing.Color.Transparent;
             this.labelScore.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelScore.ForeColor = System.Drawing.Color.RoyalBlue;
-            this.labelScore.Location = new System.Drawing.Point(394, 242);
+            this.labelScore.Location = new System.Drawing.Point(404, 320);
             this.labelScore.Name = "labelScore";
-            this.labelScore.Size = new System.Drawing.Size(247, 210);
+            this.labelScore.Size = new System.Drawing.Size(192, 71);
             this.labelScore.TabIndex = 6;
             this.labelScore.Text = "x";
             this.labelScore.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelScore.Click += new System.EventHandler(this.labelScore_Click);
+            this.labelScore.Paint += new System.Windows.Forms.PaintEventHandler(this.labelScore_Paint);
             // 
             // panel6
             // 
             this.panel6.BackColor = System.Drawing.Color.Transparent;
             this.panel6.BackgroundImage = global::Game.Properties.Resources.close;
             this.panel6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.panel6.Location = new System.Drawing.Point(549, 474);
+            this.panel6.Location = new System.Drawing.Point(532, 507);
             this.panel6.Name = "panel6";
             this.panel6.Size = new System.Drawing.Size(171, 51);
             this.panel6.TabIndex = 7;
             this.panel6.Paint += new System.Windows.Forms.PaintEventHandler(this.panel6_Paint);
+            this.panel6.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel6_MouseClick);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.label2.Location = new System.Drawing.Point(360, 237);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(286, 74);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Score";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.BackgroundImage = global::Game.Properties.Resources.start;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Location = new System.Drawing.Point(335, 507);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(171, 51);
+            this.panel1.TabIndex = 8;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseClick);
             // 
             // ExitMenu
             // 
@@ -75,6 +104,8 @@ namespace Game
             this.BackgroundImage = global::Game.Properties.Resources.exitMenuWorld;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1062, 673);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.panel6);
             this.Controls.Add(this.labelScore);
             this.Controls.Add(this.label1);
@@ -93,5 +124,7 @@ namespace Game
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label labelScore;
         private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
     }
 }
